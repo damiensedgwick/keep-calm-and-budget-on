26d2e7ec9a5db9c30d82d56c9f9dbcc979697628
Damien Sedgwick <damienksedgwick@gmail.com>
2024-03-09 20:16:29 +0000

    client side auth works but auth flash needs fixing

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,1 +1,12 @@
-<h1>Keep Calm & Budget On: Login</h1>
+<script lang="ts">
+	import { FIREBASE_AUTH } from '$lib/firebase';
+	import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+
+	async function signInWithGoogle() {
+		const provider = new GoogleAuthProvider();
+		signInWithPopup(FIREBASE_AUTH, provider);
+	}
+</script>
+
+<h2>Sign In</h2>
+<button class="w-1/2 btn btn-primary" on:click={signInWithGoogle}>Sign in with Google</button>
