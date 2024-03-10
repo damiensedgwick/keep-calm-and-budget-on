5add70d3fbe9dd44497ac33ef2bc20917f64b15f
Damien Sedgwick <damienksedgwick@gmail.com>
2024-03-10 20:59:01 +0000

    deleted it all... what is broken?

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,5 @@
 <script>
-	import { user } from '$lib/firebase';
-
 	import '../app.css';
-
-	$user;
 </script>
 
 <slot />
--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -1,19 +1,6 @@
 <script lang="ts">
-	import { goto } from '$app/navigation';
-	import AuthCheck from '$lib/components/AuthCheck.svelte';
-	import { FIREBASE_AUTH } from '$lib/firebase';
-	import { signOut as firebaseSignout } from 'firebase/auth';
-
-	async function signOut() {
-		await firebaseSignout(FIREBASE_AUTH);
-
-		goto('/');
-	}
 </script>
 
-<AuthCheck>
-	<div class="flex flex-col items-center justify-center w-screen h-screen space-y-2">
-		<h1>Welcome!</h1>
-		<button class="w-[200px] btn btn-primary" on:click={signOut}>Sign out</button>
-	</div>
-</AuthCheck>
+<div class="flex flex-col items-center justify-center w-screen h-screen space-y-2">
+	<h1>Welcome!</h1>
+</div>
