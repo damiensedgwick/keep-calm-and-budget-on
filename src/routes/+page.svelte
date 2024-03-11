3c5d9bf7c542bb8ea157b9a4f7aee376e972d83a
Damien Sedgwick <damienksedgwick@gmail.com>
2024-03-11 19:21:12 +0000

    changed link color and hover style

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,5 +4,5 @@
 </svelte:head>
 
 <div class="flex flex-col items-center justify-center w-screen h-full space-y-2">
-	<a href="/dashboard" class="link link-primary">Dashboard</a>
+	<a href="/dashboard" class="link link-hover">Dashboard</a>
 </div>
