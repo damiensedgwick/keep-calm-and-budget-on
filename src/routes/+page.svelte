93a1020f1c1812b7e8e3818eb0974913fa5b003f
Damien Sedgwick <damienksedgwick@gmail.com>
2024-03-11 19:17:17 +0000

    added title and meta

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,8 @@
+<svelte:head>
+  <title>Keep Calm & Budget On | Envelope Budgeting | Budget Planner</title>
+  <meta name="description" content="Keep Calm & Budget On brings the classic envelope budgeting method to your fingertips. Track expenses, set spending limits and visualise your financial progress with our intuitive web app." />
+</svelte:head>
+
 <div class="flex flex-col items-center justify-center w-screen h-full space-y-2">
 	<a href="/dashboard" class="link link-primary">Dashboard</a>
 </div>
