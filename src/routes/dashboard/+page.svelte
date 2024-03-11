5d828a7384fcf25d2c0323a526ff754dade9ef2f
Damien Sedgwick <damienksedgwick@gmail.com>
2024-03-11 08:52:13 +0000

    added simple check on dash for user status

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -1,6 +1,13 @@
 <script lang="ts">
+	import { user } from '$lib/firebase';
 </script>
 
 <div class="flex flex-col items-center justify-center w-screen h-screen space-y-2">
 	<h1>Welcome!</h1>
+
+	{#if $user}
+		<p>You are logged in...</p>
+	{:else}
+		<p>You are not logged in...</p>
+	{/if}
 </div>
