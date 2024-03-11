923d1de25cf28cb37ab5b001144d18e70176d986
Damien Sedgwick <damienksedgwick@gmail.com>
2024-03-11 09:08:06 +0000

    wrapped dashboard in AuthCheck

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -1,15 +1,7 @@
 <script lang="ts">
-	import { user } from '$lib/firebase';
+	import AuthCheck from '$lib/components/AuthCheck.svelte';
 </script>
 
-<div class="flex flex-col items-center justify-center w-screen h-screen space-y-2">
+<AuthCheck>
 	<h1>Welcome!</h1>
-
-	{#if $user === undefined}
-		<p>Loading...</p>
-	{:else if $user === null}
-		<p>You are not logged in...</p>
-	{:else}
-		<p>You are logged in, yay!</p>
-	{/if}
-</div>
+</AuthCheck>
