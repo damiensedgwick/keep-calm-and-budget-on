c96032575b8082bd68da8bd096e7648111b441fc
Damien Sedgwick <damienksedgwick@gmail.com>
2024-03-11 09:19:02 +0000

    swtiched to env variables

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,18 +1,18 @@
 import { initializeApp } from 'firebase/app';
-import { doc, getFirestore, onSnapshot } from 'firebase/firestore';
+import { getFirestore } from 'firebase/firestore';
 import { getAuth, onAuthStateChanged, type User } from 'firebase/auth';
 import { getStorage } from 'firebase/storage';
-import { writable, type Readable, derived } from 'svelte/store';
+import { writable } from 'svelte/store';
 import { getAnalytics } from 'firebase/analytics';
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: 'AIzaSyA2YzwXUifr5eUD_cRdR7VBJ2pC0UJENr8',
-    authDomain: 'linktree-e298a.firebaseapp.com',
-    projectId: 'linktree-e298a',
-    storageBucket: 'linktree-e298a.appspot.com',
-    messagingSenderId: '985288810204',
-    appId: '1:985288810204:web:2cde34840335795fa61e19'
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -1,7 +1,19 @@
 <script lang="ts">
+	import { goto } from '$app/navigation';
+	import { FIREBASE_AUTH } from '$lib/firebase';
+	import { signOut as firebaseSignout } from 'firebase/auth';
 	import AuthCheck from '$lib/components/AuthCheck.svelte';
+
+	async function signOut() {
+		await firebaseSignout(FIREBASE_AUTH);
+
+		goto('/');
+	}
 </script>
 
 <AuthCheck>
-	<h1>Welcome!</h1>
+	<div class="flex flex-col items-center justify-center w-screen h-screen space-y-2">
+		<h1>Welcome!</h1>
+		<button class="w-[200px] btn btn-primary text-white" on:click={signOut}>Sign out</button>
+	</div>
 </AuthCheck>
