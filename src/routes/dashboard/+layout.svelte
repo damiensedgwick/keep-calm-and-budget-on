acafbe9e485790c973a9fffba9332f7c3fa48a1c
Damien Sedgwick <damienksedgwick@gmail.com>
2024-03-12 16:09:20 +0000

    added settings page, assign pill and signout for mobile

--- src/routes/dashboard/+layout.svelte
+++ src/routes/dashboard/+layout.svelte
@@ -86,6 +86,19 @@
 		</div>
 	</div>
 
+	<!-- Authenticated Mobile Funds Pill -->
+	<div
+		class="fixed top-4 left-1/2 mx-auto -translate-x-1/2 w-60 md:hidden p-2 text-center text-white border rounded-lg navbar-center {funds >
+		0
+			? 'bg-success border-success'
+			: 'bg-error border-error'}"
+	>
+		<a href="/dashboard/budget" title="budget" class="text-lg font-bold">
+			{formatToCurrency(funds)}
+			<span class="font-normal text-regular">Needs {funds > 0 ? 'Assigning' : 'Funding'}</span>
+		</a>
+	</div>
+
 	<!-- Authenticated Global BottomNav -->
 	<div class="btm-nav md:hidden">
 		<a href="/dashboard" class={$page.route.id?.endsWith('dashboard') ? 'active' : ''}>
@@ -103,7 +116,7 @@
 					d="M3.75 3v11.25A2.25 2.25 0 0 0 6 16.5h2.25M3.75 3h-1.5m1.5 0h16.5m0 0h1.5m-1.5 0v11.25A2.25 2.25 0 0 1 18 16.5h-2.25m-7.5 0h7.5m-7.5 0-1 3m8.5-3 1 3m0 0 .5 1.5m-.5-1.5h-9.5m0 0-.5 1.5M9 11.25v1.5M12 9v3.75m3-6v6"
 				/>
 			</svg>
-			<span class="btm-nav-label"> Dashboard </span>
+			<small class="btm-nav-label"> Dashboard </small>
 		</a>
 		<a href="/dashboard/budget" class={$page.route.id?.endsWith('budget') ? 'active' : ''}>
 			<svg
@@ -121,7 +134,7 @@
 				/>
 			</svg>
 
-			<span class="btm-nav-label"> Budget </span>
+			<small class="btm-nav-label"> Budget </small>
 		</a>
 		<a
 			href="/dashboard/transactions"
@@ -142,7 +155,29 @@
 				/>
 			</svg>
 
-			<span class="btm-nav-label"> Transactions </span>
+			<small class="btm-nav-label"> Transactions </small>
+		</a>
+		<a href="/dashboard/settings" class={$page.route.id?.endsWith('settings') ? 'active' : ''}>
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				fill="none"
+				viewBox="0 0 24 24"
+				stroke-width="1.5"
+				stroke="currentColor"
+				class="w-6 h-6"
+			>
+				<path
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					d="M10.343 3.94c.09-.542.56-.94 1.11-.94h1.093c.55 0 1.02.398 1.11.94l.149.894c.07.424.384.764.78.93.398.164.855.142 1.205-.108l.737-.527a1.125 1.125 0 0 1 1.45.12l.773.774c.39.389.44 1.002.12 1.45l-.527.737c-.25.35-.272.806-.107 1.204.165.397.505.71.93.78l.893.15c.543.09.94.559.94 1.109v1.094c0 .55-.397 1.02-.94 1.11l-.894.149c-.424.07-.764.383-.929.78-.165.398-.143.854.107 1.204l.527.738c.32.447.269 1.06-.12 1.45l-.774.773a1.125 1.125 0 0 1-1.449.12l-.738-.527c-.35-.25-.806-.272-1.203-.107-.398.165-.71.505-.781.929l-.149.894c-.09.542-.56.94-1.11.94h-1.094c-.55 0-1.019-.398-1.11-.94l-.148-.894c-.071-.424-.384-.764-.781-.93-.398-.164-.854-.142-1.204.108l-.738.527c-.447.32-1.06.269-1.45-.12l-.773-.774a1.125 1.125 0 0 1-.12-1.45l.527-.737c.25-.35.272-.806.108-1.204-.165-.397-.506-.71-.93-.78l-.894-.15c-.542-.09-.94-.56-.94-1.109v-1.094c0-.55.398-1.02.94-1.11l.894-.149c.424-.07.765-.383.93-.78.165-.398.143-.854-.108-1.204l-.526-.738a1.125 1.125 0 0 1 .12-1.45l.773-.773a1.125 1.125 0 0 1 1.45-.12l.737.527c.35.25.807.272 1.204.107.397-.165.71-.505.78-.929l.15-.894Z"
+				/>
+				<path
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+				/>
+			</svg>
+			<small class="btm-nav-label"> Settings </small>
 		</a>
 	</div>
 
---
+++ src/routes/dashboard/settings/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import { goto } from '$app/navigation';
+	import { FIREBASE_AUTH } from '$lib/firebase';
+	import { signOut as firebaseSignout } from 'firebase/auth';
+
+	async function signOut() {
+		await firebaseSignout(FIREBASE_AUTH);
+
+		goto('/');
+	}
+</script>
+
+<div class="flex flex-col items-center justify-center h-4/5">
+	<button class="text-white btn btn-primary" on:click={signOut}>Sign Out</button>
+</div>
